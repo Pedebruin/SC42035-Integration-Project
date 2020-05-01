cea4dbbefe11b3764de2660b1349cf0033f2378b
Pedebruin <pedebruin@hotmail.com>
2020-05-01 16:55:17 +0200

    N4SID

--- Code/N4SID.m
+++ Code/N4SID.m
@@ -12,14 +12,10 @@ For the course:
 %}
 
 %% Settings
-
+Nx = 4;
 
 
 %% Main code
-
-
-
-
 disp('Select data file')
 [file,path]= uigetfile('*.mat');
 if isequal(file,0)
@@ -29,5 +25,50 @@ else
     load(fullfile(path,file),'h1s','h2s','t1s','t2s')
 end
 
-opts = ['InputName','Heater power'];
-data = iddata(t1s, h1s, 1, opts);
+% Append initial zeros and equilibrium temperature
+h1s = [zeros(1,Nx), h1s];
+h2s = [zeros(1,Nx), h2s];
+t1s = [ones(1,Nx)*t2s(1), t1s];
+t2s = [ones(1,Nx)*t2s(1), t2s];
+
+% Write into iddata structures
+data1 = iddata(t1s', h1s', 1, 'OutputName', 'Temperature 1', 'InputName', 'Heater power 1');
+data2 = iddata(t2s', h2s', 1, 'OutputName', 'Temperature 2', 'InputName', 'Heater power 2');
+
+% Check percistancy of exitation
+Ped1 = pexcit(data1);
+Ped2 = pexcit(data2);
+
+% Run N4SID algorithm. 
+opt= n4sidOptions('Focus','Simulation','N4Weight','MOESP');
+[Tf1,x0] = n4sid(data1,Nx,opt);
+
+
+% Simulate estimated model
+t = 1:length(t1s);
+y = lsim(Tf1,h1s,t,x0);
+
+
+
+
+% Plot
+figure 
+subplot(2,1,1)
+    hold on
+    plot(t,y)
+    plot(t,t1s)
+subplot(2,1,2)
+    hold on
+    plot(t,h1s)
+
+
+
+
+
+
+
+
+
+
+
+
