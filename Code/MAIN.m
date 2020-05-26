e46eafbe40a837e3979943c019cac4a7b16f8d5d
Pedebruin <pedebruin@hotmail.com>
2020-05-26 14:26:14 +0200

    disconnected validation from estimation, can be done completely separately now

--- Code/MAIN.m
+++ Code/MAIN.m
@@ -14,18 +14,34 @@ For the course:
 %% ==== SETUP: ====
 % ---- Accessing folders: ----
 addpath('System_Identification')
+addpath('System_Identification/Models')
 
 % ---- Settings: ----
-Identification = 1;         % Identify models?
+Identification = 0;         % Identify models?
+Validation = 1;             % Validate models?
     maken4sid   = 1;        % Use N4SID?
     makeFDSID   = 1;        % Use FDSID?
     makeGreyBox = 1;        % Use Grey Box?
+makeFigure = 1;             % Plot the result?
     
-Validation = 0;             % Validate models?
-    makeFigure = 1;         % Plot the result?
+system = 'mimo';            % siso 1, siso 2, mimo?
 
 
 
+
+
+% Store chosen options in cell array for plotting later. 
+k = 1;
+methods = cell(1,3);
+for i = {'maken4sid','makeFDSID','makeGreyBox'}
+    if eval(cell2mat(i)) == 1
+        methods{k} = char(i);
+        methods{k} = methods{k}(5:end);
+    else 
+        methods{k} = [];
+    end
+    k = k+1;
+end
 
 %% ==== IDENTIFICATION: ====
 if Identification
@@ -46,21 +62,6 @@ if Identification
         % bandwidth is about 7e-4 Hz.
     idd_i.y = lowpass(idd_i.y,5e-3,1);
 
-
-
-    % Store chosen options in cell array for plotting later. 
-    k = 1;
-    methods = cell(1,3);
-    for i = {'maken4sid','makeFDSID','makeGreyBox'}
-        if eval(cell2mat(i)) == 1
-            methods{k} = char(i);
-            methods{k} = methods{k}(5:end);
-        else 
-            methods{k} = [];
-        end
-        k = k+1;
-    end
-
     % ---- Arrays to store simulation data: ----
     ydata = [];
     T0 = mean(idd_i.y(1:55,:),1);
@@ -69,7 +70,7 @@ if Identification
         % ---- N4SID: ---- 
         disp('Estimation: N4SID')
         n4sid_settings.nx = 6;
-        n4sid_settings.system = 'mimo';   % siso 1, siso 2, mimo          
+        n4sid_settings.system = system;   % siso 1, siso 2, mimo          
         n4sid_settings.Ts = idd_i.Ts;  
         n4sid_settings.T0 = T0;
 
@@ -82,7 +83,7 @@ if Identification
         disp('Estimation: FDSID')
 
         % ---- FDSID: ----
-        FDSID_settings.system = 'mimo'; % siso 1, siso 2, mimo
+        FDSID_settings.system = system; % siso 1, siso 2, mimo
         FDSID_settings.T0 = T0;
         FDSID_settings.Ts = idd_i.Ts; 
         [sys_FDSID] = FDSID(idd_i, FDSID_settings);
@@ -92,7 +93,7 @@ if Identification
 
     if makeGreyBox
         disp('Estimation: GreyBox')
-        GreyBox_settings.system = 'mimo';     % siso 1, siso 2, mimo   
+        GreyBox_settings.system = system;     % siso 1, siso 2, mimo   
         GreyBox_settings.T0 = T0;
         % ---- Setup initial parameters: ----
         % Initial values:
@@ -143,6 +144,8 @@ if Validation
         lengthExperiment = Experiment.LengthExperiment;
     end
     tdata = 0:lengthExperiment-1;
+    T0 = mean(idd_v.y(1:55,:),1);
+
     
     % Filtering
     idd_v.y = lowpass(idd_v.y,5e-3,1);
@@ -151,23 +154,52 @@ if Validation
 
     % ---- Simulate systems using validation dataset: ----
     if maken4sid
+        
+        % if it is not generated previously, select it from other models. 
+        if ~exist('sys_n4sid')
+            disp('select n4sid model')
+            [file,path]= uigetfile('System_Identification/Models/n4sid*.mat');
+            if isequal(file,0)
+                error('No model has been selected')
+            else
+                disp(['     User selected: ', file]);
+                load(fullfile(path,file),'sys_n4sid');
+                x0_n4sid = sys_n4sid.Report.Parameters.X0;
+            end
+        end
+            
         disp('Simulating n4sid')
-        switch n4sid_settings.system(end)
+        switch system(end)
             case '1'
                 y = lsim(sys_n4sid, idd_v.u(:,1), tdata, x0_n4sid);
-                n4sid_Sim = [y + T0_n4sid', zeros(length(y),1)];  
+                n4sid_Sim = [y + T0(1), zeros(length(y),1)];  
             case '2'
                 y = lsim(sys_n4sid, idd_v.u(:,2), tdata, x0_n4sid);
-                n4sid_Sim = [zeros(length(y),1), y + T0_n4sid'];
+                n4sid_Sim = [zeros(length(y),1), y + T0(2)'];
             case 'o'
                 y = lsim(sys_n4sid, idd_v.u, tdata, x0_n4sid);
-                n4sid_Sim = y + T0_n4sid';
+                n4sid_Sim = y + T0;
         end   
     end
 
+    
+    
     if makeFDSID
+        
+        % if it is not generated previously, select it from other models. 
+        if ~exist('sys_FDSID')
+            disp('select FDSID model')
+            [file,path]= uigetfile('System_Identification/Models/fdsid*.mat');
+            if isequal(file,0)
+                error('No model has been selected')
+            else
+                disp(['     User selected: ', file]);
+                load(fullfile(path,file),'sys_FDSID');
+            end
+        end
+        
         disp('Simulating FDSID')
-        switch FDSID_settings.system(end)
+        switch system(end)
             case '1'
                 y = lsim(sys_FDSID, idd_v.u(:,1), tdata);
                 FDSID_Sim = [y + T0(1), zeros(length(y),1)];  
@@ -180,9 +212,24 @@ if Validation
         end   
     end
 
+    
+    
     if makeGreyBox
+        
+        % if it is not generated previously, select it from other models. 
+        if ~exist('sys_GreyBox')
+            disp('select GreyBox model')
+            [file,path]= uigetfile('System_Identification/Models/GreyBox*.mat');
+            if isequal(file,0)
+                error('No model has been selected')
+            else
+                disp(['     User selected: ', file]);
+                load(fullfile(path,file),'sys_GreyBox');
+            end
+        end
+        
         disp('Simulating GreyBox')
-        switch GreyBox_settings.system(end)
+        switch system(end)
             case '1'
                 y = sim(sys_GreyBox, idd_v(:,1,1), T0');
                 GreyBox_Sim = [y.y(:), zeros(length(y.y),1)];  
@@ -196,6 +243,11 @@ if Validation
     end
 
 
+    
+    
+    
+    
+    
     % Evaluate performance of simulated systems and plot
     for i = 1:length(methods)                   % for every possible method
         method = methods{i};
